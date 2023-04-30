c030382326ec1d20faca3fbe600c55e4798d8851
alvez2g <joeltiago00745@gmail.com>
2023-04-30 18:43:24 -0300

    config eslint and prettier

--- src/app/Providers/RouteServiceProvider.js
+++ src/app/Providers/RouteServiceProvider.js
@@ -2,6 +2,7 @@ import { userRouter } from "../../routes/user";
 
 class RouteServiceProvider {
     handle(app) {
+        // eslint-disable-next-line no-unused-vars
         app.get("/", (req, res, next) => {
             return res.status(200).json({
                 message: "API ON!",
